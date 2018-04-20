87ba80fd4630c1d0f7795e31fc1a88db8bd5265a
hep <hhhhep@gmail.com>
2018-04-20 18:07:09 +0900

    angular with typescript development

---
+++ auction/app/components/application/application.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from "@angular/core";
+
+@Component({
+    selector : 'auction-application',
+    templateUrl : 'app/components/application/application.component.html',
+    styleUrls : ['app/components/application/application.component.css'],
+    encapsulation : ViewEncapsulation.None
+})
+
+export default class ApplicationComponent {}
