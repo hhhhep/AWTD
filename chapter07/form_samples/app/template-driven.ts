6f80d5f651a8ea58112661a6fdd46dfb9903b7bb
itaewon <hhhhep@gmail.com>
2018-06-22 19:29:51 +0900

    ~357p

--- chapter07/form_samples/app/template-driven.ts
+++ chapter07/form_samples/app/template-driven.ts
@@ -5,9 +5,25 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector : 'app',
-    template : `test`
+    template : `
+    <html>
+        <form #f="ngForm" (ngSubmit)="onSubmit(f.value)">
+            <div>Username : <input type="text" name="username" ngModel></div>
+            <div>SSN : <input type="text" name="ssn" ngModel></div>
+            <div ngModelGroup="passwordsGroup">
+                <div>Password : <input type="password" name="password" ngModel></div>
+                <div>Confirm password : <input type="password" name="pconfirm" ngModel></div>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    </html>
+    `
 })
-class AppComponent {}
+class AppComponent {
+    onSubmit(formValue : any) {
+        console.log(formValue);
+    }
+}
 
 @NgModule({
     imports : [ BrowserModule, FormsModule, ReactiveFormsModule ],
