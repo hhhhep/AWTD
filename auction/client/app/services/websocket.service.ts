78d7668991c90749c1573cc8fd4f9324adb7466c
itaewon <hhhhep@gmail.com>
2018-07-24 19:03:22 +0900

    ~469p

---
+++ auction/client/app/services/websocket.service.ts
@@ -0,0 +1,25 @@
+import { Observable } from 'rxjs/Observable';
+import { Subscriber } from 'rxjs/Subscriber';
+
+export class WebSocketService {
+    private ws : WebSocket;
+
+    createObservableSocket(url : string, openSubscriber : Subscriber) {
+        this.ws = new WebSocket(url);
+
+        return new Observable(observer => {
+            this.ws.onmessage = event => observer.next(event.data);
+            this.ws.onerror = event => observer.error(event);
+            this.ws.onclose = event => observer.complete();
+            this.ws.onopen = event => {
+                openSubscriber.next();
+                openSubscriber.complete();
+            };
+            return () => this.ws.close();
+        });
+    }
+
+    send(message : any) {
+        this.ws.send(JSON.stringify(message));
+    }
+}
