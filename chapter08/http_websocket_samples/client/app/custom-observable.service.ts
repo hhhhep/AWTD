e9f1ec63fbfc1a077c6fa65aafe4e23154cd20e6
이태원 <hhhhep@gmail.com>
2018-10-16 09:15:59 +0900

    ~448p

--- chapter08/http_websocket_samples/client/app/custom-observable.service.ts
+++ chapter08/http_websocket_samples/client/app/custom-observable.service.ts
@@ -2,10 +2,20 @@ import { Observable } from 'rxjs/Rx';
 
 export class CustomObservableService {
     createObservableService() : Observable<Date> {
+        // return new Observable((observer) => {
+        //     setInterval(() => {
+        //         observer.next(new Date())
+        //     }, 1000);
+        // });
         return new Observable((observer) => {
-            setInterval(() => {
-                observer.next(new Date())
-            }, 1000);
+            try {
+                throw('Got an error');
+            } catch (err) {
+                observer.error(err);
+            } finally {
+                observer.complete();
+            }
         });
+        
     }
 }
